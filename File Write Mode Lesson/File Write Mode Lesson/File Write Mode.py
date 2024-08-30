83c3cbb2d9b17f57ff72a0081e12946bb5aa6d68
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-30 13:32:45 -0700

    exported from Skill Struck

---
+++ File Write Mode Lesson/File Write Mode Lesson/File Write Mode.py
@@ -0,0 +1,11 @@
+file = open("example.txt", "w")
+file.write("In short, I love to code!")
+file.close()
+
+file = open("example.txt", "r")
+print(file.read())
+file.close() 
+
+file = open("porcupine.txt", "w")
+
+
