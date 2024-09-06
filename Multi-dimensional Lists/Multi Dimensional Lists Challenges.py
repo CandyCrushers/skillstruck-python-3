e00b82c7e95976add73494ff0f8f0f31dc207cd8
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-09-06 13:54:46 -0700

    exported from Skill Struck

--- Multi-dimensional Lists/Multi Dimensional Lists Challenges.py
+++ Multi-dimensional Lists/Multi Dimensional Lists Challenges.py
@@ -1,17 +1,52 @@
 # Secret Agent Name Generator 
 
+'''first_names = ["Blaze", "Shadow", "Phoenix", "Raven"]
+last_names = ["Steel", "Storm", "Knight", "Wolf"]
 
-name = ["Java", "Python", "JavaScript", "Lua"]
-last = ["py", "ja", "js", "l"]
+agent_names = []
+
+for first in first_names:
+    name_combinations = []
+    
+    for last in last_names:
+        name_combinations.append(first + " " + last)
+    
+    agent_names.append(name_combinations)
+
+print(agent_names)'''
+
+
+# ----------------------------------------------------------------------
+
+# Fruit Blender 
+
+
+'''fruit_nam = input("Enter fruits").split()
+combos = ["apple", "grape", "peach", "cinnamon", "vanilla"]
 list = []
 
-for i in name: 
-    for y in last: 
-        full_name = i + " " + y
-        list.append(full_name)
+for y in fruit_nam: 
+    combo = []
+    for i in combos:
+        full_combo = y + " " + i
+        combo.append(full_combo)
+    list.append(combo)
 
+print(list)'''
 
+# ----------------------------------------------------------------------
 
-print(list) 
+# Subtracting in a 2D List
 
+'''rows = [int(n) for n in input("Input a list of numbers").split()]
+cols = [2,5,10,100]
+list = []
 
+for x in rows: 
+    row = []
+    for y in cols: 
+        substract = x - y 
+        row.append(substract)
+    list.append(row)
+
+print(list)'''
