af84bc0c73be37b7723cfb9ba47074309091d042
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-09-06 13:54:53 -0700

    exported from Skill Struck

---
+++ Stacks/Stacks Challenges.py
@@ -0,0 +1,48 @@
+# Stack Scramble 
+
+'''first = "r"
+
+second = "t"
+
+third = "s"
+
+fourth = "y"
+
+fifth = "o"
+
+stack = []
+
+stack.append(third)
+stack.append(second)
+stack.append(fifth)
+stack.append(first)
+stack.append(fourth)
+
+print(stack)'''
+
+# ----------------------------------------------------------------------
+
+
+# Foods with the Letter S
+
+'''answer = ["apples", "steak", "potatoes", "carrots"]
+
+add = input("Add a thing or something")
+
+if "s" in add: 
+    answer.append(add)
+    print(answer)
+else: 
+    print("The input doesn't have the letter s")'''
+
+# ----------------------------------------------------------------------
+
+# World Makeover 
+
+answer = ["c", "r", "o", "a", "k"]
+
+answer.pop()
+answer.pop()
+answer.append("m")
+answer.append("e")
+print(answer)
