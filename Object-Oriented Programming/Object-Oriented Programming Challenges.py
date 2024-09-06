7785cce6f4e06e121bf33d56575ba895461a3042
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-09-06 13:54:49 -0700

    exported from Skill Struck

---
+++ Object-Oriented Programming/Object-Oriented Programming Challenges.py
@@ -0,0 +1,37 @@
+
+# Antique Car Show
+
+'''class Car: 
+    def __init__(self, kind,color,year):
+        self.kind = kind 
+        self.color = color 
+        self.year = year'''
+
+
+# ---------------------------------------------------------------------
+
+# Arborist 
+
+
+'''class Tree: 
+    def __init__(self, fruit,height,leaf,flower,seed):
+        self.fruit = fruit 
+        self.height = height 
+        self.leaf = leaf 
+        self.flower = flower 
+        self.seed = seed '''
+
+
+# ---------------------------------------------------------------------
+
+# Bake Sale
+
+'''class Dessert: 
+    def __init__(self, shape, size, filling, topping): 
+        self.shape = shape 
+        self.size = size 
+        self.filling = filling 
+        self.topping = topping''' 
+
+
+
