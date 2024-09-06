53a1e0aa9c9a746d5c7e714fae5878a37ce1f01d
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-09-06 13:54:51 -0700

    exported from Skill Struck

---
+++ Object Continued/Objects Continued .py
@@ -0,0 +1,16 @@
+class Vacation: 
+    def __init__(self, place, distance, weather):
+        self.place = place 
+        self.distance = distance 
+        self.weather = weather
+
+    def tuesday(self): 
+        print("we will be hiking on Tuesday")
+
+summer = Vacation("Hawaii", 2000, "Sunny")
+
+summer.rating = 10
+summer.weather = "rainy"
+print(summer)
+print(summer.rating)
+print(summer.weather)
